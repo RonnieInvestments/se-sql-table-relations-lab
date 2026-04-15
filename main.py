7a04e4edf9a358c245dd7e1ad9eaf8f2f9e7843e
RonnieInvestments <ronnieinvestments81@gmail.com>
2026-04-15 14:28:00 +0300

    Add close database.

--- main.py
+++ main.py
@@ -1,195 +1,230 @@
 # STEP 0
 
-# SQL Library and Pandas Library
+# Import SQL Library and Pandas Library
 import sqlite3
 import pandas as pd
 
 # Connect to the database
-conn = sqlite3.connect('data.sqlite')
+conn = sqlite3.connect("data.sqlite")
 
 pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 
+
 # STEP 1
 # Return the first and last names and the job titles for all employees in Boston.
-df_boston = """ 
-SELECT firstName, jobTitle
-    FROM employees
+q_boston = """ 
+SELECT 
+    firstName,
+    jobTitle
+FROM employees
     JOIN offices
     USING(officeCode)
-    WHERE city = 'Boston'
+WHERE city = 'Boston'
 ; """
+
+df_boston = pd.read_sql(q_boston, conn)
 
 # STEP 2
 # Return all offices that have zero employees
-df_zero_emp = """ 
+q_zero_emp = """ 
 SELECT officeCode, city, COUNT(*) as num_employees
-    FROM employees
+FROM employees
     JOIN offices
     USING(officeCode)
-    GROUP BY officeCode
-    HAVING num_employees = 0
+GROUP BY officeCode
+HAVING num_employees = 0
 ; """
+
+df_zero_emp = pd.read_sql(q_zero_emp, conn)
 
 # STEP 3
-# Replace None with your code
 """ 
-Return:
-    employees' first name and last name
-    the city and state of the office that they work out of (if they have one).
-Include: all employees
-    order them by their first name, 
-    then their last name.
-"""
-df_employee = """ 
+Return the employees' first name and last 
+name along with the city and state of the 
+office that they work out of (if they have one).
+Include all employees and order them by their first name, 
+then their last name.
+ """
+
+q_employee = """ 
 SELECT firstName, lastName, city, state
-    FROM employees
+FROM employees
     JOIN offices
     USING(officeCode)
-    ORDER BY firstName, lastName
+ORDER BY firstName, lastName
 ; """
 
+df_employee = pd.read_sql(q_employee, conn)
+
 # STEP 4
-""" 
-Return all:
-    the customer's contact information (first name, last name, and phone number) 
-    their sales rep's employee number for any customer who has not placed an order.
+""" Return all of the customer's contact information 
+(first name, last name, and phone number) as 
+well as their sales rep's employee number for any customer 
+who has not placed an order.
 Sort the results alphabetically based on the contact's last name
 """
-df_contacts = """ 
-SELECT contactFirstName, contactLastName, phone, salesRepEmployeeNumber
-    FROM customers
+
+q_contacts = """ 
+SELECT 
+    contactFirstName, 
+    contactLastName, 
+    phone, 
+    salesRepEmployeeNumber
+FROM customers
     LEFT JOIN orders
     USING(customerNumber)
-    WHERE orderNumber IS NULL
-    ORDER BY contactLastName
+WHERE orderNumber IS NULL
+ORDER BY contactLastName
 ; """
+
+df_contacts = pd.read_sql(q_contacts, conn)
 
 # STEP 5
 """ 
-Return all:
-    the customer contacts (first and last names) 
-    details for each of the customers' payment 
-    amounts and dates of payment.
+Return all the customer contacts (first and last names) 
+along with details for each of the customers' payment 
+amounts and dates of payment.
 Results to be sorted in descending order by the payment amount
 ; """
-df_payment = """ 
-SELECT contactFirstName, contactLastName,
-    CAST(amount AS FLOAT) AS amount_float,paymentDate
-    FROM customers
+
+q_payment = """ 
+SELECT 
+    contactFirstName, 
+    contactLastName,
+    CAST(amount AS FLOAT) AS amount_float,
+    paymentDate
+FROM customers
     JOIN payments
     USING(customerNumber)
-    ORDER BY amount_float DESC
+ORDER BY amount_float DESC
 ; """
+
+df_payment = pd.read_sql(q_payment, conn)
 
 # STEP 6
 """
-Return the employee:
-    number, first name, last name, and number of customers for employees 
-    whose customers have an average credit limit over 90k.
+Return the employee number, first name, last name, and number of customers for employees 
+whose customers have an average credit limit over 90k.
 Sort by number of customers from high to low.
 ; """
-df_credit = """ 
-SELECT firstName,lastName,
+q_credit = """ 
+SELECT 
+    firstName,
+    lastName,
     COUNT(*) AS num_customers,
     AVG(CAST(creditLimit AS FLOAT)) AS avg_credit_limit
-    FROM employees AS e
+FROM employees AS e
     JOIN customers AS c
     ON e.employeeNumber = c.salesRepEmployeeNumber
-    GROUP BY 1,2
-    HAVING avg_credit_limit > 90000
-    ORDER BY num_customers DESC
+GROUP BY 1,2
+HAVING avg_credit_limit > 90000
+ORDER BY num_customers DESC
 ; """
+
+df_credit = pd.read_sql(q_credit, conn)
 
 # STEP 7
 """
-Return the:
-    product name and count the number of orders for each 
-    product as a column named 'numorders'.
-Also return:
-    a new column, 'totalunits', that sums up the total quantity of product 
-    sold (use the 'quantityOrdered column')
+Return the product name and count the number of orders for each 
+product as a column named 'numorders'.
+Also return a new column, 'totalunits', that sums up the total quantity of product 
+sold (use the 'quantityOrdered column')
 """
-df_product_sold = """ 
-SELECT productName,
+q_product_sold = """ 
+SELECT
+    productName,
     COUNT(*) AS numorders,
     SUM(quantityOrdered) AS totalunits
-    FROM products
+FROM products
     JOIN orderDetails
     USING(productCode)
-    GROUP BY 1
-    ORDER BY totalunits DESC
+GROUP BY 1
+ORDER BY totalunits DESC
 ; """
+df_product_sold = pd.read_sql(q_product_sold, conn)
 
 # STEP 8
 """
-Return:
-    the product name, code, 
-    and the total number of customers who have ordered each product, 
-    aliased as 'numpurchasers'
+Return the product name, code, 
+and the total number of customers who have ordered each product, 
+aliased as 'numpurchasers'
+
 Sort the results by the highest number of purchasers.
 """
-df_total_customers = """ 
-SELECT p.`productName`, p.`productCode`,
+q_total_customers = """ 
+SELECT 
+    p.`productName`,
+    p.`productCode`,
     COUNT(DISTINCT c.`customerName`) AS numpurchasers
-    FROM products AS p
+FROM products AS p
     JOIN orderdetails AS od
         ON p.`productCode` = od.`productCode`
     JOIN orders AS o
         ON od.`orderNumber` = o.`orderNumber`
     JOIN customers AS c
         ON o.`customerNumber` = c.`customerNumber`
-    GROUP BY 1
-    ORDER BY numpurchasers DESC
+GROUP BY 1
+ORDER BY numpurchasers DESC
 ; """
+df_total_customers = pd.read_sql(q_total_customers, conn)
 
 # STEP 9
 """ 
-Find out number of customers per office.
-Return:
-    the count as a column named 'n_customers'.
-    the office code and city.
+Find out how many customers there are per office.
+Return the count as a column named 'n_customers'.
+Also, return the office code and city.
 """
-df_customers = """ 
-SELECT o.`officeCode`, o.city,
+q_customers = """ 
+SELECT 
+    o.`officeCode`,
+    o.city,
     COUNT(c.`customerNumber`) AS n_customers
-    FROM offices AS o
+FROM offices AS o
     JOIN employees AS e
       ON o.`officeCode` = e.`officeCode`
     JOIN customers AS c
       ON e.`employeeNumber` = c.`salesRepEmployeeNumber`
-    GROUP BY 1
+GROUP BY 1
 ; """
+df_customers = pd.read_sql(q_customers, conn)
 
 # STEP 10
 """
-Using a subquery, select:
-    the employee number, 
-    first name, last name, 
-    city of the office, and 
-    the office code for employees who sold 
-    products that have been ordered by fewer than 20 customers.
+Using a subquery, select the employee number, 
+first name, last name, city of the office, and the office code for employees who sold 
+products that have been ordered by fewer than 20 customers.
 """
-df_under_20 = """ 
-SELECT employeeNumber, firstName, lastName, city, officeCode
-    FROM (
-        SELECT 
-            *,
-            COUNT(DISTINCT c.`customerName`) AS numpurchasers
-            FROM products AS p
-            JOIN orderdetails AS od
-                ON p.`productCode` = od.`productCode`
-            JOIN orders AS o
-                ON od.`orderNumber` = o.`orderNumber`
-            JOIN customers AS c
-                ON o.`customerNumber` = c.`customerNumber`
-            JOIN employees AS e
-                ON c.`salesRepEmployeeNumber` = e.`employeeNumber`
-            JOIN offices AS off
-                ON e.`officeCode` = off.`officeCode`
-            GROUP BY employeeNumber
-            HAVING numpurchasers < 20
-        )
-    ORDER BY lastName
-    ; """
 
+q_under_20 = """ 
+SELECT 
+    employeeNumber,
+    firstName,
+    lastName,
+    city,
+    officeCode
+FROM (
+      SELECT 
+        *,
+        COUNT(DISTINCT c.`customerName`) AS numpurchasers
+      FROM products AS p
+          JOIN orderdetails AS od
+              ON p.`productCode` = od.`productCode`
+          JOIN orders AS o
+              ON od.`orderNumber` = o.`orderNumber`
+          JOIN customers AS c
+              ON o.`customerNumber` = c.`customerNumber`
+          JOIN employees AS e
+              ON c.`salesRepEmployeeNumber` = e.`employeeNumber`
+          JOIN offices AS off
+              ON e.`officeCode` = off.`officeCode`
+      GROUP BY employeeNumber
+      HAVING numpurchasers < 20
+)
+ORDER BY lastName
+; """
+
+df_under_20 = pd.read_sql(q_under_20, conn)
+
+
+#Close the connection to the database
 conn.close()
